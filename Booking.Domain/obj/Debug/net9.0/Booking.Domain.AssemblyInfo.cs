0f9befa0b7ab9b69fd4fee5c76145d8f7285cf4d
Santio13-code <smora9480@gmail.com>
2025-10-18 20:26:34 -0500

    Pagina de inicio

--- Booking.Domain/obj/Debug/net9.0/Booking.Domain.AssemblyInfo.cs
+++ Booking.Domain/obj/Debug/net9.0/Booking.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Booking.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70c1369ddeda5738edd1cd1fd68031b9f41ffe06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86dc5399f2f85cec2571d65fc31208471b2256fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Booking.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Booking.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
