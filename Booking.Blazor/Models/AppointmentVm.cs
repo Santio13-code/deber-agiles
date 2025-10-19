5213303ba4cbdef61506c2ca8dc4a892b51b1f3e
Santio13-code <smora9480@gmail.com>
2025-10-18 19:18:42 -0500

    se mejoro el disenio de las paginas

--- Booking.Blazor/Models/AppointmentVm.cs
+++ Booking.Blazor/Models/AppointmentVm.cs
@@ -1,3 +1,6 @@
 namespace Booking.Blazor.Models;
-public record AppointmentVm(Guid Id, Guid ClientId, Guid ServiceId, DateTime StartUtc, DateTime EndUtc, byte Status, string? Notes);
+public record AppointmentVm(Guid Id, Guid ClientId, Guid ServiceId, DateTime StartUtc, DateTime EndUtc, byte Status, string? Notes){
+    public string? ClientName { get; set; }   // 👈 NUEVO
+    public string? ServiceName { get; set; }  // 👈 NUEVO
+};
 public record CreateAppointmentVm(Guid ClientId, Guid ServiceId, DateTime StartUtc, string? Notes);
